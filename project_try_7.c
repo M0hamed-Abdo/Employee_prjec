de199cfd3bef48ca31ab75969b4aa11767d33f7d
M0hamed-Abdo <moh3m7ed@gmail.com>
2025-08-04 11:52:42 +0200

    Update project_try_7.c

--- project_try_7.c
+++ project_try_7.c
@@ -8,52 +8,49 @@ struct employee {
     float salary;
     char location[30];
 };
-
-struct employee *emp=NULL;
-int count =0;
-void add_employee() {
-    emp = realloc(emp,(count +1)* sizeof(struct employee));
-    printf("----RECORD EMPLOYEE #%d----\n",count+1);
+void add_employee(struct employee **emp,int *count) {
+    *emp = realloc(*emp,(*count +1)* sizeof(struct employee));
+    printf("----RECORD EMPLOYEE #%d----\n",*count+1);
     printf("Enter Employee Id: ");
-    scanf("%d",&emp[count].id);
-    for(int i=0;i<count;i++){
-        if(emp[i].id == emp[count].id){
+    scanf("%d",&(*emp)[*count].id);
+    for(int i=0;i<*count;i++){
+        if((*emp)[i].id == (*emp)[*count].id){
            printf("This ID Already Use.\n");
            return;
         }
     }
     printf("Enter Employee Name: ");
-    scanf("%s",&emp[count].name);
+    scanf("%s",&(*emp)[*count].name);
     printf("Enter Employee salary: ");
-    scanf("%f",&emp[count].salary);
+    scanf("%f",&(*emp)[*count].salary);
     printf("Enter Employee location: ");
-    scanf("%s",&emp[count].location);
+    scanf("%s",&(*emp)[*count].location);
     printf("\n");
     printf("----DONE RECORD----\n");
-    count++;
+    (*count)++;
     return;
 
 
 }
 
-void add_multiple_employee() {
-    int employee_count;
+void add_multiple_employee(struct employee **emp,int *count) {
+    int count_multiple_employee;
     printf("enter employee count:");
-    scanf("%d",&employee_count);
-    for(int i=0;i<employee_count;i++){
-            add_employee();
+    scanf("%d",&count_multiple_employee);
+    for(int i=0;i<count_multiple_employee;i++){
+            add_employee(emp,count);
     }
 }
 
-void update_employee() {
+void update_employee(struct employee *emp,int *count) {
     int id_for_update;
-    if(count == 0){
+    if(*count == 0){
        printf("----NO EMPLOYEE RECORD---\n");
        return;
     }
        printf("Enter id for update:");
        scanf("%d",&id_for_update);
-       for(int i=0;i<count;i++){
+       for(int i=0;i<*count;i++){
         if(emp[i].id == id_for_update){
             printf("Enter New Name: ");
             scanf("%s",&emp[i].name);
@@ -68,20 +65,20 @@ void update_employee() {
              printf("Employee not found!!!\n");
 }
 
-void delete_employee() {
+void delete_employee(struct employee *emp,int *count) {
     int id_for_delete;
-    if(count == 0){
+    if(*count == 0){
        printf("----NO EMPLOYEE RECORD---\n");
        return;
     }
         printf("Enter id for Delete: ");
         scanf("%d",&id_for_delete);
-        for(int i=0;i<count;i++) {
+        for(int i=0;i<*count;i++) {
             if(emp[i].id == id_for_delete) {
-                for(int j=i;j<count-1;j++) {
+                for(int j=i;j<*count-1;j++) {
                      emp[j]=emp[j+1];
                 }
-                count--;
+                (*count)--;
                 printf("Done Delete\n");
                 return;
             }
@@ -89,15 +86,15 @@ void delete_employee() {
             printf("No found Employee\n");
 }
 
-void serch_employee() {
+void serch_employee(struct employee *emp,int *count) {
     int serch_by_id;
-    if(count == 0){
+    if(*count == 0){
        printf("----NO EMPLOYEE RECORD---\n");
        return;
     }
     printf("Enter id for serch:");
     scanf("%d",&serch_by_id);
-    for(int i=0;i<count;i++) {
+    for(int i=0;i<*count;i++) {
         if(serch_by_id == emp[i].id) {
             printf("ID: %d\n",emp[i].id);
             printf("NAME: %s\n",emp[i].name);
@@ -109,13 +106,14 @@ void serch_employee() {
          printf("employee not found!!!\n");
 }
 
-void get_all() {
-    if(count == 0){
+void get_all(struct employee *emp,int *count) {
+    if(*count == 0){
        printf("----NO EMPLOYEE RECORD---\n");
        printf("\n");
-    }else{
+       return;
+    }
        printf("-----All Employee-----\n");
-       for(int i=0;i<count;i++) {
+       for(int i=0;i<*count;i++) {
         printf("EMPLOYEE NO:%d\n",i+1);
         printf("ID: %d\n", emp[i].id);
         printf("NAME: %s\n",emp[i].name);
@@ -124,10 +122,10 @@ void get_all() {
         printf("----------------------\n");
        }
     }
-    return;
-}
 
 void minue() {
+    struct employee *emp=NULL;
+    int count =0;
     int choice;
     while (1){
     printf("\n");
@@ -146,22 +144,22 @@ void minue() {
 
     switch(choice) {
     case 1:
-        add_employee();
+        add_employee(&emp,&count);
         break;
     case 2:
-        add_multiple_employee();
+        add_multiple_employee(&emp,&count);
         break;
     case 3:
-        update_employee();
+        update_employee(emp,&count);
         break;
     case 4:
-        delete_employee();
+        delete_employee(emp,&count);
         break;
     case 5:
-        serch_employee();
+        serch_employee(emp,&count);
         break;
     case 6:
-        get_all();
+        get_all(emp,&count);
         break;
     case 0:
         printf("Exiting...\n");
@@ -173,8 +171,6 @@ void minue() {
         }
     }
 }
-
-
 int main() {
 
     minue();
